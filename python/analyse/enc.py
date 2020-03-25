025dfdafe94f203334158a70f2816e0d5fb73936
evan <chenwenguang@vkansee.com>
2020-03-25 11:54:18 +0800

    fixed a bug

--- python/analyse/enc.py
+++ python/analyse/enc.py
@@ -3,15 +3,35 @@ import sys
 
 def FileConvert(szOldefile, szNewfile):
     odf = open(szOldefile, "rb")
-    with open(szNewfile, "wb") as ndf:
-        while(odf.readable()):
-            c = odf.read(1) ^ 0xa9
-            ndf.write(c)
+    with open(szNewfile, "w") as ndf:
+        c = odf.read(1)        
+        ndf.write(chr(ord(c)))
+        while True:
+            c = odf.read(1)
+            if not c: break
+            c = ord(c) ^ 0xa9
+            ndf.write(chr(c))
     odf.close()
+
+def FileEncoding(szSrcFile,szEncFile):
+    sf = open(szSrcFile, "rb")
+    with open(szEncFile, "wb") as edf:
+        c = sf.read(1)
+        edf.write(c)
+        while True:
+            c = sf.read(1)
+            if not c: break
+            c = ord(c) ^ 0xa9
+            edf.write(bytes([c]))
+    sf.close()
 
 
 if __name__ == "__main__":
     szOldefile = "E:/work/learn/python/analyse/config.en"
     szNewfile = "E:/work/learn/python/analyse/config.ini"
-    FileConvert(szOldefile,szNewfile)
+    szEncfile = "E:/work/learn/python/analyse/config1.en"
+    szNewfile2 = "E:/work/learn/python/analyse/config1.ini"
+    #FileConvert(szOldefile,szNewfile)
+    FileEncoding(szNewfile, szEncfile)
+    FileConvert(szEncfile,szNewfile2)
     pass
