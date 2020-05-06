2e9b02d4c4acfd23e2486dee5e129282de4d0e56
chenwen202 <chenwen202@163.com>
2020-05-06 23:01:43 +0800

    chooseMenu function

--- web/website/class_web/task/final/js/index.js
+++ web/website/class_web/task/final/js/index.js
@@ -1,4 +1,39 @@
 
 function chooseMenu(obj) {
+	var element = document.getElementById('content');
+	if( !element.hasChildNodes()) return;
 
+	var list = element.childNodes;
+	var name = "";
+	switch(parseInt(obj.value))
+	{
+		case 0:
+			name="miaodui";
+		break;
+		case 1:
+			name = "shiqu";
+		break;
+		default:
+			name = "jieshao";
+		break;
+	}		
+	console.log("name",name);
+
+	for (var i = list.length - 1; i >= 0; i--) {
+		if(list[i].id == null) continue;
+
+		console.log("id",i,list[i].id);
+		if(list[i].id == name )
+		{
+			if(list[i].hidden)
+				list[i].hidden =false;
+		}		
+		else
+		{
+			if( !list[i].hidden )
+				list[i].hidden = true;
+		}
+	};
+
+	
 }
